3e8f8aafa35d47961c2ab13bb411b383af6b4568
Lin Jiang <lnjng07@protonmail.com>
2024-07-28 02:44:59 -0400

    render text

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use sdl2::event::Event;
+use sdl2::rect::Rect;
 use sdl2::render::Canvas;
+use sdl2::surface::Surface;
 use sdl2::pixels::Color;
+use sdl2::ttf::{self, Font, Sdl2TtfContext};
 
 fn main() {
     // initialize SDL2
@@ -10,8 +13,16 @@ fn main() {
         .position_centered()
         .build()
         .unwrap();
-    let mut canvas = window.into_canvas().build().unwrap();
 
+    let mut canvas = window.into_canvas().build().unwrap();
+    let texture = canvas.texture_creator();
+
+    // initialize fonts
+    let ttf_context: Sdl2TtfContext = ttf::init()
+        .expect("Failed to initialize TTF context");
+    let font: Font = ttf_context.load_font("/usr/local/share/fonts/ttf/Fira/FiraSans-Regular.ttf", 16)
+        .expect("Failed to load font");
+    let mut text_input: String = String::from("");
 
     // create event loop
     let mut event_pump = sdl_context.event_pump().unwrap();
@@ -19,12 +30,24 @@ fn main() {
         // draw things
         canvas.set_draw_color(Color::WHITE);
         canvas.clear();
+        if text_input.len() > 0 {
+            let text_surface: Surface = font
+                .render(&text_input)
+                .blended(Color::BLACK)
+                .expect("Failed to render text");
+            let text_texture = text_surface.as_texture(&texture);
+            let dimensions = font.size_of(&text_input).unwrap();
+            canvas.copy(&text_texture.unwrap(), None, Some(Rect::new(100, 100, dimensions.0, dimensions.1))).unwrap();
+        }
         canvas.present();
 
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} => {
                     break 'running
+                },
+                Event::TextInput { text, .. } => {
+                    text_input += &text;
                 },
                 _ => {}
             }
