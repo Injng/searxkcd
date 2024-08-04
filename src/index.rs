d87ab486691e26e47fa897001a96a3c7756db0ba
Lin Jiang <lnjng07@protonmail.com>
2024-08-04 07:17:10 -0400

    add ability to get trancsripts and save in json

--- src/index.rs
+++ src/index.rs
@@ -1,12 +1,11 @@
 use dirs;
 use reqwest;
-use scraper::{Html, Selector};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::fs;
 use std::path::Path;
 
-#[derive(Serialize)]
+#[derive(Deserialize, Serialize)]
 pub struct Comic {
     id: u64,
     title: String,
@@ -61,6 +60,7 @@ pub fn get_home() -> String {
 
 /// Initializes the index by creating the necessary directories
 pub fn init_index() {
+    // get home directory of system
     let home = get_home();
 
     // create index directory
@@ -74,6 +74,15 @@ pub fn init_index() {
         let index: Vec<Comic> = Vec::new();
         let index_json = serde_json::to_string(&index).unwrap();
         fs::write(index_file, index_json).expect("Failed to create index file");
+    }
+
+    // check if error file exists; if not, create one
+    let error_path = format!("{}/.local/share/searxkcd/error.json", home);
+    let error_file = Path::new(&error_path);
+    if !error_file.exists() {
+        let error: Vec<u64> = Vec::new();
+        let error_json = serde_json::to_string(&error).unwrap();
+        fs::write(error_file, error_json).expect("Failed to create error file");
     }
 }
 
@@ -91,19 +100,37 @@ async fn get_latest_id() -> u64 {
 }
 
 /// Gets the comic specified by the id
-pub async fn get_comic(id: u64) -> Comic {
-    // check if id is valid
-    let latest_id: u64 = get_latest_id().await;
-    if id == 0 || id > latest_id {
-        panic!("Invalid comic id");
+pub async fn get_comic(id: u64) -> Result<Comic, &'static str> {
+    // create reqwest client and target URL
+    let client = reqwest::Client::new();
+    let api_url: String = format!("https://xkcd.com/{}/info.0.json", id);
+
+    // get comic info from XKCD api, retrying when necessary
+    let mut body: String = "".to_string();
+    let mut is_error = true;
+    for _ in 0..3 {
+        body = match client.get(&api_url).send().await {
+            Ok(response) => response.text().await.unwrap(),
+            Err(_) => continue,
+        };
+        is_error = false;
+        break;
     }
 
-    // otherwise, get basic comic info from XKCD api
-    let api_url: String = format!("https://xkcd.com/{}/info.0.json", id);
-    let body: String = reqwest::get(&api_url).await.unwrap().text().await.unwrap();
+    // check if failed
+    if is_error {
+        println!("Error: failed retrieving info for #{}", id);
+        return Err("Error: failed retrieving comic");
+    }
 
     // deserialize JSON into a Comic
-    let comic: Value = serde_json::from_str(&body).unwrap();
+    let comic: Value = match serde_json::from_str(&body) {
+        Ok(comic) => comic,
+        Err(_) => {
+            println!("Error: failed deserializing info for #{}", id);
+            return Err("Error: failed deserializing comic");
+        }
+    };
     let comic_id: u64 = comic["num"].as_u64().unwrap();
     let comic_url: String = comic["img"].as_str().unwrap().to_string();
     let comic_title: String = comic["title"].as_str().unwrap().to_string();
@@ -118,45 +145,86 @@ pub async fn get_comic(id: u64) -> Comic {
         .await
         .unwrap();
 
-    // find index of transcript heading
-    let transcript_idx: usize = body.find("Transcript").expect("Transcript not found");
-    let after_transcript: String = body[transcript_idx..].to_string();
-    let after_idx: usize = after_transcript.find("<dl>").expect("dl not found");
-    let before_idx: usize = after_transcript
-        .find("<span id=\"Discussion\">")
-        .expect("Span not found");
-    let transcript_snippet: String = after_transcript[after_idx..before_idx].to_string();
-
-    // parse transcript snippet
-    let document = Html::parse_fragment(&transcript_snippet);
-    let dl_selector = Selector::parse("dl").unwrap();
-    let dd_selector = Selector::parse("dd").unwrap();
-
-    // get transcript items and write to String
-    let transcript_html = document.select(&dl_selector);
+    // get transcript snippet from html
     let mut transcript: String = "".to_string();
-    for items in transcript_html {
-        for item in items.select(&dd_selector) {
-            transcript += &item.text().collect::<String>();
-            transcript += " ";
+    let mut failed = false;
+    let after_idx: usize = match body.find("id=\"Transcript\"") {
+        Some(idx) => idx,
+        None => {
+            failed = true;
+            0
         }
+    };
+    let before_idx: usize = match body.find("id=\"Discussion\"") {
+        Some(idx) => idx,
+        None => {
+            failed = true;
+            0
+        }
+    };
+    if !failed {
+        transcript = body[after_idx..before_idx].to_string();
     }
 
     // make and return the Comic
-    Comic::new(comic_id, comic_title, comic_alt, comic_url, transcript)
+    Ok(Comic::new(
+        comic_id,
+        comic_title,
+        comic_alt,
+        comic_url,
+        transcript,
+    ))
 }
 
 /// Update index
-pub async fn update_index(comics: &mut Vec<Comic>) {
-    let latest_id: u64 = get_latest_id().await;
-    let latest_comic: Comic = get_comic(latest_id).await;
-    println!("Latest comic: {}", latest_comic.transcript);
-    comics.push(latest_comic);
-
-    // update index file
+pub async fn update_index() -> Vec<Comic> {
+    // get path to index and error files
     let home: String = get_home();
     let index_path: String = format!("{}/.local/share/searxkcd/index.json", home);
     let index_file = Path::new(&index_path);
-    let index_json: String = serde_json::to_string(&comics).unwrap();
+    let error_path: String = format!("{}/.local/share/searxkcd/error.json", home);
+    let error_file = Path::new(&error_path);
+
+    // read json into vector
+    let index_json: String = fs::read_to_string(index_file).expect("Failed to read index file");
+    let mut index: Vec<Comic> = serde_json::from_str(&index_json).unwrap();
+    let error_json: String = fs::read_to_string(error_file).expect("Failed to read error file");
+    let errors: Vec<u64> = serde_json::from_str(&error_json).unwrap();
+
+    // retry failed comics
+    let mut new_errors: Vec<u64> = Vec::new();
+    for id in errors {
+        let comic: Comic = match get_comic(id).await {
+            Ok(comic) => comic,
+            Err(_) => {
+                new_errors.push(id);
+                continue;
+            }
+        };
+        index.push(comic);
+    }
+
+    // get last id in index file; if no comics in index file, set to zero
+    let last_id: u64 = (index.len() + new_errors.len()) as u64;
+    let next_id: u64 = last_id + 1;
+
+    // get comics that are not in index file
+    for id in next_id..get_latest_id().await {
+        let comic: Comic = match get_comic(id).await {
+            Ok(comic) => comic,
+            Err(_) => {
+                new_errors.push(id);
+                continue;
+            }
+        };
+        index.push(comic);
+    }
+
+    // update index and error files
+    let index_json: String = serde_json::to_string(&index).unwrap();
     fs::write(index_file, index_json).expect("Failed to create index file");
+    let error_json: String = serde_json::to_string(&new_errors).unwrap();
+    fs::write(error_path, error_json).expect("Failed to create error file");
+
+    index
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 pub mod index;
 
-use index::{Comic, init_index, update_index};
+use index::{init_index, update_index, Comic};
 
 use sdl2::event::Event;
 use sdl2::image::{InitFlag, LoadTexture};
+use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::{Texture, TextureQuery};
 use sdl2::surface::Surface;
-use sdl2::pixels::Color;
 use sdl2::ttf::{self, Font, Sdl2TtfContext};
 use tokio;
 
@@ -17,7 +17,8 @@ async fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let _image_context = sdl2::image::init(InitFlag::PNG).unwrap();
-    let window = video_subsystem.window("SearXKCD", 1440, 900)
+    let window = video_subsystem
+        .window("SearXKCD", 1440, 900)
         .position_centered()
         .build()
         .unwrap();
@@ -25,14 +26,13 @@ async fn main() {
     let texture = canvas.texture_creator();
 
     // initialize xkcd index
-    let mut comics: Vec<Comic> = Vec::new();
     init_index();
-    update_index(&mut comics).await;
+    let mut comics: Vec<Comic> = update_index().await;
 
     // initialize fonts
-    let ttf_context: Sdl2TtfContext = ttf::init()
-        .expect("Failed to initialize TTF context");
-    let font: Font = ttf_context.load_font("/usr/local/share/fonts/ttf/Fira/FiraSans-Regular.ttf", 16)
+    let ttf_context: Sdl2TtfContext = ttf::init().expect("Failed to initialize TTF context");
+    let font: Font = ttf_context
+        .load_font("/usr/local/share/fonts/ttf/Fira/FiraSans-Regular.ttf", 16)
         .expect("Failed to load font");
     let mut text_input: String = String::from("");
 
@@ -49,12 +49,19 @@ async fn main() {
                 .expect("Failed to render text");
             let text_texture = text_surface.as_texture(&texture);
             let dimensions = font.size_of(&text_input).unwrap();
-            canvas.copy(&text_texture.unwrap(), None, Some(Rect::new(100, 100, dimensions.0, dimensions.1))).unwrap();
+            canvas
+                .copy(
+                    &text_texture.unwrap(),
+                    None,
+                    Some(Rect::new(100, 100, dimensions.0, dimensions.1)),
+                )
+                .unwrap();
         }
 
         // get latest xkcd
-        let img_path: String = comics[0].download_img().await;
-        let img_texture: Texture = texture.load_texture(img_path)
+        let img_path: String = comics[comics.len() - 1].download_img().await;
+        let img_texture: Texture = texture
+            .load_texture(img_path)
             .expect("Failed to load image");
         let img_info: TextureQuery = img_texture.query();
         let img_rect: Rect = Rect::new(200, 200, img_info.width, img_info.height);
@@ -63,12 +70,10 @@ async fn main() {
 
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} => {
-                    break 'running
-                },
+                Event::Quit { .. } => break 'running,
                 Event::TextInput { text, .. } => {
                     text_input += &text;
-                },
+                }
                 _ => {}
             }
         }
