9c23cf7a2d11bcae77b5fb48358aabc7561fe4ed
Lin Jiang <lnjng07@protonmail.com>
2024-08-05 16:40:51 -0400

    add utils for scaling rectangles

--- src/utils.rs
+++ src/utils.rs
@@ -3,7 +3,7 @@ use sdl2::rect::Rect;
 use sdl2::surface::Surface;
 use sdl2::ttf::Font;
 
-// render text with a specific font at a specific location on the canvas
+/// Render text with a specific font at a specific location on the canvas
 pub fn render_text(
     canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
     texture_creator: &sdl2::render::TextureCreator<sdl2::video::WindowContext>,
@@ -25,4 +25,24 @@ pub fn render_text(
             Some(Rect::new(x, y, dimensions.0, dimensions.1)),
         )
         .unwrap();
+}
+
+/// Scales the given dimensions down into a Rect
+pub fn fit_rect(width: u32, height: u32, rect: Rect) -> Rect {
+    // get dimensions of rect
+    let rect_width: u32 = rect.width();
+    let rect_height: u32 = rect.height();
+
+    // calculate scaling factors and select the biggest one
+    let width_scale: f32 = rect_width as f32 / width as f32;
+    let height_scale: f32 = rect_height as f32 / height as f32;
+    let scale: f32 = width_scale.max(height_scale);
+
+    // return new scaled Rect
+    Rect::new(
+        rect.x(),
+        rect.y(),
+        (rect_width as f32 * scale) as u32,
+        (rect_height as f32 * scale) as u32,
+    )
 }
